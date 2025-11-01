0bfe1254292877fe40bab15df18dad223718ceb7
Estelamb <Estelamb@users.noreply.github.com>
2025-11-01 13:27:07 +0000

    deploy: ca932ce85f93894c1c0c1eeb6629cc3f0918d801

--- structsystem__context.js
+++ structsystem__context.js
@@ -1,6 +1,7 @@
 var structsystem__context =
 [
     [ "brightness", "structsystem__context.html#a9009362f41ad83f73535239e36aad2e3", null ],
+    [ "brightness_sem", "structsystem__context.html#ab47d276fd3142c120d71707a90d27522", null ],
     [ "mode", "structsystem__context.html#a497903b46418329ff77e573fe793a374", null ],
     [ "phototransistor", "structsystem__context.html#a74975195688a51fa8ccb033b4a75dbe0", null ]
 ];
